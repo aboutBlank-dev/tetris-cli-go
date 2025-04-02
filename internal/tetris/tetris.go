f7705014e6936b124588c3a5deadd9a8d8134c23
aboutBlank-dev <josecolaco1999@gmail.com>
2025-04-03 00:14:42 +0100

    snapshot

--- internal/tetris/tetris.go
+++ internal/tetris/tetris.go
@@ -1,7 +1,41 @@
 package tetris
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	"golang.org/x/term"
+)
+
+func resetTerminal() {
+	fmt.Print("\033[0m")  // Reset colors
+	fmt.Print("\033[?25h") // Show cursor
+}
 
 func Start() {
-	fmt.Println("Tetris game started!")
+	//Set raw mode, disable on refer
+	oldTerm, err := term.MakeRaw(int(os.Stdin.Fd()))
+	if err != nil {
+		panic(err)
+	}
+	defer term.Restore(int(os.Stdin.Fd()), oldTerm) 
+
+	defer resetTerminal() // Ensure reset when the program exits
+
+	for {
+		var b = make([]byte, 1) 
+		_, err := os.Stdin.Read(b)
+		if err != nil {
+			fmt.Println("Failed to read input. Ignoring: ", err)
+			continue
+		}
+
+		key := b[0]
+		if key == 'q' {
+			fmt.Println("Quitting...")
+			break
+		}
+	}
+
+	// defer term.Restore(int(os.Stdin.Fd()), oldState) // Restore the terminal state when done
 }
